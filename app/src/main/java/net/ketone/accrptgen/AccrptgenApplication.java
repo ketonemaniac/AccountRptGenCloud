cb825207340e168620611b8482c54fe5aca787bd
ketonemaniac <ketonemaniac@gmail.com>
2019-03-01 23:41:50 +0800

    Lazy Initialization to speed up boot time

--- app/src/main/java/net/ketone/accrptgen/AccrptgenApplication.java
+++ app/src/main/java/net/ketone/accrptgen/AccrptgenApplication.java
@@ -3,6 +3,9 @@ package net.ketone.accrptgen;
 import org.apache.poi.util.POILogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class AccrptgenApplication {
@@ -11,5 +14,10 @@ public class AccrptgenApplication {
 //		System.setProperty("org.apache.poi.util.POILogger", "org.apache.poi.util.SystemOutLogger" );
 //		System.setProperty("poi.log.level", POILogger.DEBUG + "");
 		SpringApplication.run(AccrptgenApplication.class, args);
+	}
+
+	@Configuration
+	@ComponentScan(lazyInit = true)
+	static class LocalConfig {
 	}
 }
