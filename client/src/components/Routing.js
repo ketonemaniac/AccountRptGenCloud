777cd0ab537376bd64745eb9a0291513666009a3
Ketone Maniac <ketonemaniac@gmail.com>
2021-05-10 23:09:10 +0800

    login page and logo font change

--- client/src/components/Routing.js
+++ client/src/components/Routing.js
@@ -4,22 +4,23 @@ import App from './home/App';
 import UserAdmin from './useradmin/UserAdmin.js'
 import Settings from './settings/Settings.js'
 import Frame from './Frame.js';
+import Login from './login/Login';
 
-class Routing extends Component {
 
-    render() {
-        return (
+const Routing = () => {
+
+    return (
             <Router>
                 <Route exact path="/">
                     <Redirect to='/app/main' />
                 </Route>
+                <Route exact path="/login" component={Login} />
                 <Route path="/app" component={Frame} />
                 <Route path="/app/users" component={UserAdmin} />
                 <Route path="/app/settings" component={Settings} />
                 <Route path="/app/main" component={App} />
             </Router>
-        );
-    }
+    );
 }
 
 export default Routing; 
